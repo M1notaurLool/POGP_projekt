680889de8a60059ee8e996f969359c3e734dc7ad
M1notaurLool <m.troscak15@gmail.com>
2025-04-02 10:58:28 +0200

    Vylepšený pohyb rakiet #26

--- sever/games.py
+++ sever/games.py
@@ -1,33 +1,6 @@
 import pygame
 from networks import Network
-
-
-class Player():
-    width = height = 50
-
-    def __init__(self, startx, starty, color=(255,0,0)):
-        self.x = startx
-        self.y = starty
-        self.velocity = 2
-        self.color = color
-
-    def draw(self, g):
-        pygame.draw.rect(g, self.color ,(self.x, self.y, self.width, self.height), 0)
-
-    def move(self, dirn):
-        """
-        :param dirn: 0 - 3 (right, left, up, down)
-        :return: None
-        """
-
-        if dirn == 0:
-            self.x += self.velocity
-        elif dirn == 1:
-            self.x -= self.velocity
-        elif dirn == 2:
-            self.y -= self.velocity
-        else:
-            self.y += self.velocity
+from player import Player
 
 
 class Game:
@@ -36,8 +9,8 @@ class Game:
         self.net = Network()
         self.width = w
         self.height = h
-        self.player = Player(50, 50)
-        self.player2 = Player(100,100)
+        self.player = Player(250, 250)
+        self.player2 = Player(100, 100)
         self.canvas = Canvas(self.width, self.height, "Testing...")
 
     def run(self):
@@ -50,26 +23,16 @@ class Game:
                 if event.type == pygame.QUIT:
                     run = False
 
-                if event.type == pygame.K_ESCAPE:
-                    run = False
-
             keys = pygame.key.get_pressed()
 
-            if keys[pygame.K_RIGHT]:
-                if self.player.x <= self.width - self.player.velocity:
-                    self.player.move(0)
-
-            if keys[pygame.K_LEFT]:
-                if self.player.x >= self.player.velocity:
-                    self.player.move(1)
-
-            if keys[pygame.K_UP]:
-                if self.player.y >= self.player.velocity:
-                    self.player.move(2)
-
-            if keys[pygame.K_DOWN]:
-                if self.player.y <= self.height - self.player.velocity:
-                    self.player.move(3)
+            if keys[pygame.K_RIGHT]:  # Otáčanie doprava
+                self.player.move(3)
+            if keys[pygame.K_LEFT]:  # Otáčanie doľava
+                self.player.move(2)
+            if keys[pygame.K_UP]:  # Pohyb dopredu
+                self.player.move(0)
+            if keys[pygame.K_DOWN]:  # Cúvanie
+                self.player.move(1)
 
             # Send Network Stuff
             self.player2.x, self.player2.y = self.parse_data(self.send_data())
@@ -87,7 +50,7 @@ class Game:
         Send position to server
         :return: None
         """
-        data = str(self.net.id) + ":" + str(self.player.x) + "," + str(self.player.y)
+        data = f"{self.net.id}:{self.player.x},{self.player.y}"
         reply = self.net.send(data)
         return reply
 
@@ -97,7 +60,7 @@ class Game:
             d = data.split(":")[1].split(",")
             return int(d[0]), int(d[1])
         except:
-            return 0,0
+            return 0, 0
 
 
 class Canvas:
@@ -105,22 +68,15 @@ class Canvas:
     def __init__(self, w, h, name="None"):
         self.width = w
         self.height = h
-        self.screen = pygame.display.set_mode((w,h))
+        self.screen = pygame.display.set_mode((w, h))
         pygame.display.set_caption(name)
 
     @staticmethod
     def update():
         pygame.display.update()
 
-    def draw_text(self, text, size, x, y):
-        pygame.font.init()
-        font = pygame.font.SysFont("comicsans", size)
-        render = font.render(text, 1, (0,0,0))
-
-        self.screen.draw(render, (x,y))
-
     def get_canvas(self):
         return self.screen
 
     def draw_background(self):
-        self.screen.fill((255,255,255))
+        self.screen.fill((0, 0, 0))
