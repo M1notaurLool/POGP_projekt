582446a8b460b9facbe6b3110b50ffdfd6e92b16
M1notaurLool <m.troscak15@gmail.com>
2025-04-03 21:09:34 +0200

    zmeny

--- sever/player.py
+++ sever/player.py
@@ -55,3 +55,27 @@ class Player:
     def get_hitbox(self):
         """Vráti hitbox ako pygame Rect (na kolízie)."""
         return pygame.Rect(self.x - 25, self.y - 25, self.WIDTH, self.HEIGHT)
+
+
+class Bullet:
+    SIZE = 8  # Veľkosť strely
+
+    def __init__(self, x, y, angle, speed):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.speed = speed
+
+    def move(self):
+        """Posunie strelu v smere uhla."""
+        radian_angle = math.radians(self.angle)
+        self.x += self.speed * math.cos(radian_angle)
+        self.y -= self.speed * math.sin(radian_angle)
+
+    def draw(self, screen):
+        """Nakreslí strelu na obrazovku."""
+        pygame.draw.circle(screen, (255, 255, 0), (int(self.x), int(self.y)), self.SIZE)
+
+    def get_hitbox(self):
+        """Vráti hitbox strely."""
+        return pygame.Rect(self.x - self.SIZE // 2, self.y - self.SIZE // 2, self.SIZE, self.SIZE)
