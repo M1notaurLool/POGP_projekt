0f78d794fa18be98e540c827171d9276b17d63b1
Sebastian <sebsim037@gmail.com>
2025-04-04 15:55:16 +0200

    #32 Nefunkcnost multyplayer

--- sever/networks.py
+++ sever/networks.py
@@ -1,45 +1,58 @@
 import socket
-
-
+from _thread import *
 import sys
-class Network:
-    def __init__(self, okno_instance):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        #nacitanie IP address zadanej v PyQt
-        self.host = okno_instance.get_saved_address()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+server = '192.168.88.11'
+port = 5555
 
-        self.port = okno_instance.get_saved_port()
+server_ip = socket.gethostbyname(server)
 
-        self.addr = (self.host, self.port)
-        self.id = self.connect()
+try:
+    s.bind((server, port))
 
+except socket.error as e:
+    print(str(e))
 
-    #Funkcia ktora kontorluje ci je klient pripojeny alebo nie
-    def connect(self):
+s.listen(2)
+print("Waiting for a connection")
+
+currentId = "0"
+pos = ["0:50,50", "1:100,100"]
+def threaded_client(conn):
+    global currentId, pos
+    conn.send(str.encode(currentId))
+    currentId = "1"
+    reply = ''
+    while True:
         try:
-            self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            data = conn.recv(2048)
+            reply = data.decode('utf-8')
+            if not data:
+                conn.send(str.encode("Goodbye"))
+                break
+            else:
+                print("Recieved: " + reply)
+                arr = reply.split(":")
+                id = int(arr[0])
+                pos[id] = reply
+
+                if id == 0: nid = 1
+                if id == 1: nid = 0
+
+                reply = pos[nid][:]
+                print("Sending: " + reply)
+
+            conn.sendall(str.encode(reply))
         except:
-            from games import Game
-            print(f"Pri pripajani nastala chyba skontrolujte udaje na pripojenie na server.")
-            Game.close_game()
-            return "ERROR"
+            break
 
+    print("Connection Closed")
+    conn.close()
 
-    def send(self, data):
-        """
-        :param data: str
-        :return: str
-        """
-        try:
-            #odoslanie dat cez soket
-            self.client.send(str.encode(data))
-            #prijatie odpocede od servera
-            reply = self.client.recv(2048).decode()
-            #vrati odpoved sercera
-            return reply
-        except socket.error as e:
-            #Ak nastane chyba vrati spravu o chybe
-            return str(e)
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+
+    start_new_thread(threaded_client, (conn,))
