faf80f9b524b4588b6bfdc35d41244f05f5ad89e
Sebastian <sebsim037@gmail.com>
2025-04-14 19:49:21 +0200

    #34 multy

--- player.py
+++ player.py
@@ -1,25 +1,29 @@
 import pygame
 import math
-from bullet import Bullet
+
 
 class Player():
     width = height = 50
 
-    def __init__(self, startx, starty, color=(255,0,0)):
+    def __init__(self, startx, starty, color=(255,0,0), image_path="player.png"):
         self.x = startx
         self.y = starty
         self.velocity = 2
         self.color = color
 
+        # Načítanie obrázka a zmenšenie na veľkosť hráča
+        self.image = pygame.image.load("obrazok/RaketaPassive.png")
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+
     def draw(self, g):
-        pygame.draw.rect(g, self.color ,(self.x, self.y, self.width, self.height), 0)
+        # Vykreslí obrázok na pozíciu (x, y)
+        g.blit(self.image, (self.x, self.y))
 
     def move(self, dirn):
         """
         :param dirn: 0 - 3 (right, left, up, down)
         :return: None
         """
-
         if dirn == 0:
             self.x += self.velocity
         elif dirn == 1:
