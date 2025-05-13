5cee664c0be2eec446194191d53f83bb0f4108b6
M1notaurLool <m.troscak15@gmail.com>
2025-05-13 13:28:33 +0200

    #61 pohyb boostov, opravena shieeld boost,boosty synchronizácia

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 import socket
 from _thread import *
-from share import Share
+import time
+
+# Boost management
+boosts = []  # Shared boost positions and types
+taken_boost_ids = set()  # To keep track of used boosts
+
+# Add this if using custom boost sync
+def generate_boosts():
+    return [
+        "heal:600,300",
+        "shield:800,500",
+        "turbo:1000,400"
+    ]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
 server = "127.0.0.1"
-
-
-
 port = 11000
-
 server_ip = socket.gethostbyname(server)
 
 try:
@@ -22,34 +28,43 @@ s.listen(2)
 print("Waiting for a connection")
 
 currentId = "0"
-pos = ["0:50,50,0|", "1:100,100,0|"]  # Pridaný aj angle a strely
+pos = ["0:50,50,0||50", "1:100,100,0||50"]
 
-def threaded_client(conn):
-    global currentId, pos
-    conn.send(str.encode(currentId))
-    currentId = "1"
+def threaded_client(conn, player_id):
+    global currentId, pos, boosts, taken_boost_ids
+
+    conn.send(str.encode(str(player_id)))
     reply = ''
     while True:
         try:
             data = conn.recv(2048)
-            reply = data.decode('utf-8')
             if not data:
-                conn.send(str.encode("Goodbye"))
                 break
-            else:
-                arr = reply.split(":")
-                id = int(arr[0])
-                pos[id] = reply  # Celý string od hráča
 
-                nid = 1 - id  # identifikácia druhého hráča
-                conn.sendall(str.encode(pos[nid]))
+            reply = data.decode("utf-8")
+            if reply.startswith("BOOST_TAKEN:"):
+                boost_id = reply.split(":")[1]
+                taken_boost_ids.add(boost_id)
+                continue
+
+            arr = reply.split(":")
+            id = int(arr[0])
+            pos[id] = reply
+
+            nid = 1 - id
+            response = f"{pos[nid]}~{';'.join(boosts)}~{','.join(taken_boost_ids)}"
+            conn.sendall(str.encode(response))
         except:
             break
 
     print("Connection Closed")
     conn.close()
 
+# Prepare shared boost data before game starts
+boosts = generate_boosts()
+
 while True:
     conn, addr = s.accept()
-    print("Connected to: ", addr)
-    start_new_thread(threaded_client, (conn,))
+    print("Connected to:", addr)
+    start_new_thread(threaded_client, (conn, int(currentId)))
+    currentId = str(1 - int(currentId))
--- shield_boost.py
+++ shield_boost.py
@@ -1,5 +1,9 @@
 from boost import Boost
 
 class Shield(Boost):
+    def __init__(self, image_path, x, y):
+        super().__init__(image_path, x, y)
+        self.id = f"shield:{x},{y}"
+
     def apply_to_player(self, player):
         player.activate_shield()
